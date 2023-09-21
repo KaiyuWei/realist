6bb20e40cbca47b0634211fa98eccb65edade3ea
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-09-21 19:01:22 +0200

    email containing activation link completed

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,6 +3,7 @@
  */
 
 import * as config from '../config.js';
+import jwt from "jsonwebtoken";
 
 export const welcome = (req, res) => {
     res.json({
@@ -13,17 +14,43 @@ export const welcome = (req, res) => {
 export const preRegister = async (req, res) => {
     // generate a clickable link containing jwt for authenticating the email address
     // only when this link is clicked, the registration completes
-    console.log(req.body);
-    AWSSES.sendEmail(
+    
+    // the email and password from the request body
+    const {email, password} = req.body;
+    const token = jwt.sign({email, password}, config.JWT_SECRET, {
+        expiresIn: "1h",
+    });
+    config.AWSSES.sendEmail(
     {
         Source: config.EMAIL_FROM,
         // a hard-coded email address for developing
-        Destination: "weikaiyu2020@outlook.com",
-    }, 
+        Destination: {
+            ToAddresses: ["weikaiyu2020@outlook.com"],
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data:`
+                        <html>
+                            <h1>Welcome to Realist</h1>
+                            <p>Click the link below to activate your account</p>
+                            <a href="${config.CLIENT_URL}/auth/account-activate/${token}">Activate account</a>
+                        </html>
+                    `,
+                },
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: "Welcome to Realist",
+            }
+        },
+    },  
     (err, data) => {
+        // console.log("REGION: " + config.apiVersion);
         if (err) {
            console.log(err);
-            return res.jsoon({ok: false});
+            return res.json({ok: false});
         }
         else {
            console.log(data);
@@ -31,3 +58,11 @@ export const preRegister = async (req, res) => {
         }
     });
 };
+
+export const register = async (req, res) => {
+    try {
+        console.log(req.body);
+    } catch (err) {
+
+    }
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,8 +7,10 @@ import * as auth from "../controllers/auth.js";
 const router = express.Router();
 
 // for get requests in '/' endpoint
-router.get('/', auth.welcome);
+router.get("/", auth.welcome);
 // the middleware users before authenticating their email address
 router.post("/pre-register", auth.preRegister);
+// for destructure the email activation link and create a user.
+router.post("/register", auth.register);
 
 export default router;
