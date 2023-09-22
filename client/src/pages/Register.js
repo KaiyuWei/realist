7b3b8ee85d722fbbfddf8eebf6345a8e0ca84ada
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-09-22 22:57:04 +0200

    registeration page progress

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 export default function Register() {
+  // the hooks for states
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // submission handler
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      console.table([email, password]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <h1 className="display-1 bg-primary text-light p-5">Register</h1>
@@ -6,12 +22,26 @@ export default function Register() {
       <div className="container">
         <div className="row">
           <div className="col-lg-4 offset-lg-4">
-            <form>
+            <form onSubmit={handleSubmit}>
               <input
                 type="text"
                 placeholder="Enter your email"
-                className="form-control"
+                className="form-control mb-4"
+                required
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
+              <input
+                type="password"
+                placeholder="Enter your email"
+                className="form-control mb-4"
+                required
+                autoFocus
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button className="btn btn-primary col-12 mb-4">Register</button>
             </form>
           </div>
         </div>
