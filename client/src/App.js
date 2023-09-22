f747165f75f8252ebf382c61fcf4155491209ca7
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-09-22 23:17:19 +0200

    access to Toast

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home.js";
 import Login from "./pages/Login.js";
 import Register from "./pages/Register.js";
 import { AuthProvider } from "./context/auth.js";
 import Main from "./components/nav/Main.js";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <BrowserRouter>
       <Main />
+      <Toaster />
       <AuthProvider>
         <Routes>
           <Route path="/" element={<Home />} />
