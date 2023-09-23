1934bfb20baa771b7c5431e09b465cbd3676987b
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-09-23 20:37:27 +0200

    add google places auto-complete

--- client/src/components/forms/AdForm.js
+++ client/src/components/forms/AdForm.js
@@ -2,7 +2,24 @@
  * a component of form that allows users to create an ad
  */
 
+import { useState } from "react";
+
 export default function AdForm({ action, type }) {
+  // state
+  const [ad, setAd] = useState({
+    photos: [],
+    uploading: false,
+    price: "",
+    address: "",
+    bedrooms: "",
+    bathrooms: "",
+    carpark: "",
+    landsize: "",
+    type: "",
+    title: "",
+    description: "",
+    loading: false,
+  });
   return (
     <>
       <p>This is ad create form</p>
